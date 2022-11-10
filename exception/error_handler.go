efe6b3e3999e5fc047a182d5fd9b70c5495527f8
Iman Nurohman <iman@gits.id>
2022-11-11 03:39:22 +0700

    add message

--- exception/error_handler.go
+++ exception/error_handler.go
@@ -9,14 +9,14 @@ func ErrorHandler(ctx *fiber.Ctx, err error) error {
 	_, ok := err.(ValidationError)
 	if ok {
 		return ctx.Status(400).JSON(model.WebResponse{
-			Code:   400,
-			Status: "BAD_REQUEST",
-			Data:   err.Error(),
+			Code:     400,
+			Status:   "BAD_REQUEST",
+			Messages: err.Error(),
 		})
 	}
 	return ctx.Status(500).JSON(model.WebResponse{
-		Code:   500,
-		Status: "INTERNAL_SERVER_ERROR",
-		Data:   err.Error(),
+		Code:     500,
+		Status:   "INTERNAL_SERVER_ERROR",
+		Messages: err.Error(),
 	})
 }
